c409cfec519a06ee05e2b615d1706234f15ca422
jamos90 <jamos_98@hotmail.com>
2018-07-08 15:39:55 +0100

    can add to customer collection

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,3 +1,5 @@
+import Instruments.Guitar;
+import Type.Family;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,11 +9,14 @@ import static org.junit.Assert.assertSame;
 public class CustomerTest {
     Customer customer;
     Customer customer2;
+    Guitar guitar;
 
     @Before
     public void setUp(){
+        guitar = new Guitar("Les Paul",500,800, Family.STRING);
         customer = new Customer("Jess",500);
         customer2 = new Customer("James",400);
+
     }
 
     @Test
@@ -27,5 +32,11 @@ public class CustomerTest {
     @Test
     public void customerStartsWithNoInstument(){
         assertEquals(0,customer.instrumentCount());
+    }
+
+    @Test
+    public void canAddToCustomersCollection(){
+        customer.addToInstrumentCollection(guitar);
+        assertEquals(1,customer.instrumentCount());
     }
 }
